29052a52d813b8298621964133574c36752f1363
qazxsw1240 <qazxsw010528@gmail.com>
2025-06-22 07:54:28 +0900

    Add main entry

---
+++ MovieBooking/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using MovieBooking.Applications;
+using MovieBooking.Services;
+
+namespace MovieBooking
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+            builder.Services
+                .AddSingleton(new IoProcessor(Console.In, Console.Out))
+                .AddDbContextCollection()
+                .AddSingleton<Application>()
+                .AddHostedService<ApplicationService>()
+                .AddLogging(builder => builder.ClearProviders());
+            using IHost app = builder.Build();
+            await app.RunAsync();
+        }
+    }
+}
